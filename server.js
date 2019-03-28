89d035b0715d8f133754a52126fd6e7724ecc681
themishkaCoder <45307191+themishkaCoder@users.noreply.github.com>
2019-03-28 20:32:07 +0200

    version 1.2.1
     1. Баны стали более мение работоспособными
     2. Список ip адресов в Админ панели

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-// const mongoose = require("mongoose");
-// const keys = require("./keys");
+//const mongoose = require("mongoose");
+//const keys = require("./keys");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require('mongodb').ObjectID
+var db;
 const app = express();
 const server = require("http").createServer(app);
 
@@ -8,14 +11,64 @@ const io = require("socket.io").listen(server);
 
 var port = process.env.PORT || 3000;
 
-server.listen(port);
-//server.listen(process.env.PORT);
+// MongoClient.connect('mongodb+srv://admin:14stsahara@cluster0-qenr5.mongodb.net/messages?retryWrites=true', function (err, database) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   db = database.db('mess');
+//   server.listen(port, function () {
+//     console.log('API app started');
+//   })
+// })
+
+//server.listen(port);
+
+
+server.listen(3000, "192.168.88.200");
+
 
 // mongoose.connect(keys.mongoURI)
 //     .then(() => console.log("MongoDB connected."))
 //     .catch(err => console.error(err))
 
 app.use(express.static('public'));
+
+
+////////////////// Mongo start
+
+// app.post('/mess', function (req, res) {
+//     var messList = {
+//       name: req.body.name
+//     };
+  
+//     db.collection('mess').insert(messList, (err, result) => {
+//       if (err) {
+//         console.log(err);
+//         return res.sendStatus(500);
+//       }
+//       res.send(messList);
+//     })
+//   })
+
+//   app.get('/mess', function (req, res) {
+//     db.collection('mess').find().toArray(function (err, docs) {
+//       if (err) {
+//         console.log(err);
+//         return res.sendStatus(500);
+//       }
+//       res.send(docs);
+//     })
+//   })
+
+
+
+
+////////////////// Mongo end
+
+
+
+
+
 
 app.get("/", (request, respons) => {
     respons.sendFile(__dirname + "/public/index.html" );
@@ -39,23 +92,40 @@ app.get("/profile", (request, respons) => {
 
 var users = [];
 var connections = [];
+var banList = [];
 
 io.sockets.on("connection", (socket) => {
     var clientIp = socket.request.connection.remoteAddress;
     var clientId = socket.id;
     console.log("Новый пользователь подключен " + clientIp + " : " + clientId);
+    
     //console.log(socket.id);
+    //connections.push(clientIp);
     connections.push(clientIp);
     console.log(connections);
+    console.log("banned users: " + banList);
     // if(clientIp == "192.168.88.31"){
     //     socket.disconnect(true);
     //     console.log("You banned! " + clientIp);
     // }
+    for(var banCheck of banList){
+        if(banCheck == clientIp){
+            socket.disconnect(true);
+        }
+    }
+    for(var connectIp of connections){
+        console.log(connectIp);
+        io.sockets.emit("newUser", connectIp);
+    }
     
 
     socket.on("disconnect", (data) => {
         connections.splice(connections.indexOf(socket), 1);
         console.log("Пользователь отключен " + clientIp);
+        console.log(connections);
+        
+        io.sockets.emit("removeUser", clientIp);
+        
     })
 
     socket.on("sendMess", (data, name) => {
@@ -72,13 +142,20 @@ io.sockets.on("connection", (socket) => {
         var banIp = {
             ip: data
         };
-        //banIp.ip.disconnect(true);
         for(var itemIp of connections){
             if(itemIp == banIp.ip){
                 //socket.disconnect(true);
                 io.sockets.emit("answer");
-                console.log("You banned! : " + banIp.ip);
+                socket.disconnect(true).to(banIp.ip);
+                connections.splice(connections.indexOf(banIp.ip), 1);
+                
+                banList.push(banIp.ip);
+                console.log("You banned! : " + banIp.ip + "\n\n" + "banned users: " + banList);
+                //console.log(connections);
+                
                 // io.sockets.emit("disconnect");
+                
+                
             }
         }
         //socket.disconnect(true);
